ffe682829abc971e4322673d710c9221b29d4fba
Alinebarbosaa <alineemille4@gmail.com>
2025-08-05 21:37:13 -0300

    part3

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
+let list = document.querySelectorAll('.item')
+let next = document.getElementById('next')
+let prev = document.getElementById('prev')
 
+// count = 3
+// 0, 1, 2
+//
+let count = list.length //3
+let active = 0
+
+next.onclick = () => {
+    let activeOld = document.querySelector('.active')
+    activeOld.classList.remove('active')
+
+    active = active + 1
+    list [active].classList.add('active')
+}
+prev.onclick = () => {
+    let activeOld = document.querySelector('.active')
+    activeOld.classList.remove('active')
+}
