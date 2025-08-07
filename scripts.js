8ad1b3e36b7543b4792c38f3e5ed5e2167bc54e9
Alinebarbosaa <alineemille4@gmail.com>
2025-08-07 18:40:01 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,18 +12,15 @@ next.onclick = () => {
     let activeOld = document.querySelector('.active')
     activeOld.classList.remove('active')
                                                       
-    active = active >= count -1 ? 0 : active +1
-    list [active].classList.add('active')
-
+    active = active >= count -1 ? 0 : active + 1
+    list[active].classList.add('active')
 
 }
 prev.onclick = () => {
     let activeOld = document.querySelector('.active')
     activeOld.classList.remove('active')
 
-     active = active <= 0 ? count -1 : active - 1
-    list[active].classList.add('active')
-
-
+    active = active <= 0 ? count -1 : active - 1
+    active[active].classList.add('active')
 
 }
